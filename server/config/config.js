941b7458b309f2a5f237718ffbc4efa6d6f8e099
Jose Fiascaro <53021912+jfiascaro@users.noreply.github.com>
2020-01-01 16:42:55 -0600

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,11 +11,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 // ===== PUEBASE DE DATOS =====
 // ============================
+
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://lobo:TW6yDYtwmBXKnwdw@cluster0-qgjvj.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 
 process.env.URLDB = urlDB;
